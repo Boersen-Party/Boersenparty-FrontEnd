04d624c79943acd62c16d8e69fec1b0f8e0753c1
selhdr <selinhidiroglu@web.de>
2024-11-06 18:04:38 +0100

    tailwindcss, eventtriggerwindow component
    Tailwindcss konfiguriert
    Komponente "Event-Trigger-Window" angelegt

--- Boersen-Party-FrontEnd/src/app/app.component.ts
+++ Boersen-Party-FrontEnd/src/app/app.component.ts
@@ -3,17 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { PriceOverviewComponent } from './price-overview/price-overview.component'
 import { CommonModule } from '@angular/common';
 import {PriceEntryTabComponent} from './price-entry-tab/price-entry-tab.component';
+import {EventTriggerWindowComponent} from './event-trigger-window/event-trigger-window.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PriceOverviewComponent, CommonModule, PriceEntryTabComponent],
+  imports: [RouterOutlet, PriceOverviewComponent, CommonModule, PriceEntryTabComponent, EventTriggerWindowComponent],
   template: `
     <section>
       <!-- Price Overview Component -->
       <app-price-overview></app-price-overview>
     </section>
-
+    // <app-event-trigger-window></app-event-trigger-window>
     <section>
       <app-price-entry-tab></app-price-entry-tab>
     </section>
