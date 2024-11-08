b673420f93e93b212eed6ddd7a8483688fed2c60
selhdr <selinhidiroglu@web.de>
2024-11-08 15:12:59 +0100

    main push for new branch

---
+++ Boersen-Party-FrontEnd/src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { PriceOverviewComponent } from './price-overview/price-overview.component'
+import { CommonModule } from '@angular/common';
+import {EventTriggerWindowComponent} from './event-trigger-window/event-trigger-window.component';
+import {PriceEntryTabComponent} from './price-entry-tab/price-entry-tab.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, PriceOverviewComponent, CommonModule, PriceEntryTabComponent, EventTriggerWindowComponent],
+  template: `
+    <div class= "main-grid">
+      <!--Grid for Price Overview Element-->
+      <div class="price-overiew-position">
+
+        <section>
+          <app-price-overview></app-price-overview>
+        </section>
+
+        <section>
+          <app-price-entry-tab></app-price-entry-tab>
+        </section>
+      </div>
+      <!-- End of Grid-->
+
+      <!--Grid for Event Trigger-->
+      <div class="event-button-position">
+        <section>
+          <app-event-trigger-window></app-event-trigger-window>
+        </section>
+      </div>
+      <!-- End of Grid-->
+
+    </div>
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Boersen-Party';
+}
