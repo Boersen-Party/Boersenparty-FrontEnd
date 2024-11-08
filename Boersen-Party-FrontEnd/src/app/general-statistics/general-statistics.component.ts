691ca567f17375f3b6f76d77ff9483128bcc1af1
Dominik <147036891+doeiit00@users.noreply.github.com>
2024-11-08 17:15:10 +0100

    Testing Statistics
    Co-Authored-By: T <149052166+tamoit02@users.noreply.github.com>

--- Boersen-Party-FrontEnd/src/app/general-statistics/general-statistics.component.ts
+++ Boersen-Party-FrontEnd/src/app/general-statistics/general-statistics.component.ts
@@ -1,42 +1,41 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
-import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CanvasJSAngularChartsModule],
+  imports: [CommonModule, RouterOutlet, Chart, registerables],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular17ssrapp';
-  chartOptions = {
-    title: {
-      text: "Angular Column Chart with Index Labels"
-    },
-    animationEnabled: true,
-    axisY: {
-      includeZero: true
-    },
-    data: [{
-      type: "column", //change type to bar, line, area, pie, etc
-      //indexLabel: "{y}", //Shows y value on all Data Points
-      indexLabelFontColor: "#5A5757",
-      dataPoints: [
-        { x: 10, y: 71 },
-        { x: 20, y: 55 },
-        { x: 30, y: 50 },
-        { x: 40, y: 65 },
-        { x: 50, y: 71 },
-        { x: 60, y: 92, indexLabel: "Highest\u2191" },
-        { x: 70, y: 68 },
-        { x: 80, y: 38, indexLabel: "Lowest\u2193"  },
-        { x: 90, y: 54 },
-        { x: 100, y: 60 }
-      ]
-    }]
+  createTotalUsersChart(): any {
+    return {
+      type: 'line',
+      data: {
+        labels: ['Jan', 'Feb', 'March', 'April'],
+        datasets: [
+          {
+            label: 'Total Users',
+            data: [12, 19, 22, 23],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    };
+  }
+
+  ngOnInit() {
+    this.totalUsers = new Chart('totalUsers', this.createTotalUsersChart());
   }
 }
