5d6cb5d49a06ff3046e9a431554ffd923ec97a79
mfnomad <mfnomad@gmail.com>
2024-11-28 17:39:30 +0100

    implemented Placeholders for Start /Enddate of Parties and Partyname also

--- Boersen-Party-FrontEnd/src/app/create-party-button-click-window/create-party-button-click-window.component.ts
+++ Boersen-Party-FrontEnd/src/app/create-party-button-click-window/create-party-button-click-window.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PartyServiceService}  from '../services/party-service.service';
 import { FormsModule } from '@angular/forms';
 import { Party } from '../_model/party';
@@ -11,10 +11,10 @@ import { Party } from '../_model/party';
     templateUrl: './create-party-button-click-window.component.html',
     styleUrls: ['./create-party-button-click-window.component.css']
 })
-export class CreatePartyButtonClickWindowComponent {
+export class CreatePartyButtonClickWindowComponent implements OnInit{
 
   constructor(public partyService: PartyServiceService) {}
-
+  
   partyname : string = '';
   start_date: string = '';
   end_date: string = '';
@@ -27,6 +27,29 @@ export class CreatePartyButtonClickWindowComponent {
     };
   
     this.partyService.createParty(party);
+
+}
+
+
+ /*
+
+sets the date Placeholders : start is today() and end is today() + 1 day
+datatype: datetime-local
+ */
+startDatePlaceholder: string = '';
+endDatePlaceholder: string = '';
+
+ngOnInit(): void {
+  this.setPlaceholders();
+}
+
+setPlaceholders() {
+  const today = new Date(); 
+  const dayLater = new Date(today);
+  dayLater.setDate(today.getDate() + 1);
+  // Format the dates to `yyyy-MM-ddTHH:mm` for `datetime-local`
+  this.start_date = today.toISOString().slice(0, 16);
+  this.end_date = dayLater.toISOString().slice(0, 16);
 
 }
 
