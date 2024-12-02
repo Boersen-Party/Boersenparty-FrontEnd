766aac581f7f640ab7b8f750a32300562174be09
mfnomad <mfnomad@gmail.com>
2024-12-02 19:46:39 +0100

    party header created

--- Boersen-Party-FrontEnd/src/app/display/display.component.ts
+++ Boersen-Party-FrontEnd/src/app/display/display.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { PartyHeaderComponent } from './party-header/party-header.component';
 
 @Component({
   selector: 'app-display',
-  imports: [],
+  imports: [PartyHeaderComponent],
   templateUrl: './display.component.html',
   styleUrl: './display.component.css'
 })
