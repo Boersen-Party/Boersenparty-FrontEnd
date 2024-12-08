751675878b6b4f0c1058c7988cebc949d587fa2a
selhdr <selinhidiroglu@web.de>
2024-12-08 16:54:16 +0100

    Personal anlegen, Grid anpassen

--- Boersen-Party-FrontEnd/src/app/personal/personal.component.ts
+++ Boersen-Party-FrontEnd/src/app/personal/personal.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {PriceOverviewComponent} from '../veranstalter/price-overview/price-overview.component';
+import {EventTriggerWindowComponent} from '../veranstalter/event-trigger-window/event-trigger-window.component';
 
 @Component({
   selector: 'app-personal',
   standalone: true,
-  imports: [],
+  imports: [PriceOverviewComponent, EventTriggerWindowComponent],
   templateUrl: './personal.component.html',
   styleUrl: './personal.component.css'
 })
