28a8db9b68155a500eb348a7096caf10581ac70e
selhdr <selinhidiroglu@web.de>
2024-12-08 16:05:12 +0100

    Price Overview Check

--- Boersen-Party-FrontEnd/src/app/personal/personal.component.ts
+++ Boersen-Party-FrontEnd/src/app/personal/personal.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-personal',
+  standalone: true,
   imports: [],
   templateUrl: './personal.component.html',
   styleUrl: './personal.component.css'
