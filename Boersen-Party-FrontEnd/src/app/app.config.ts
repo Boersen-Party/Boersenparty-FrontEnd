80443b693da403fb7c00a7baaf4aabdb941cee6c
T <tamoit02@hs-esslingen.de>
2024-12-13 14:36:35 +0100

    Keycloak Initalisierung Frontend
    Co-Authored-By: Dominik <147036891+doeiit00@users.noreply.github.com>

--- Boersen-Party-FrontEnd/src/app/app.config.ts
+++ Boersen-Party-FrontEnd/src/app/app.config.ts
@@ -1,9 +1,35 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, APP_INITIALIZER, importProvidersFrom } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser'; // add provideAnimations
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { KeycloakAngularModule } from 'keycloak-angular';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; // Konflikte mit Angular 18
+
 import { routes } from './app.routes';
 
+import { KeycloakService } from 'keycloak-angular';
+import { initializeKeycloak } from './init/keycloak-init.factory';
+
 export const appConfig: ApplicationConfig = {
-  providers: [ provideHttpClient(withFetch()), provideRouter(routes, withComponentInputBinding())]
+  providers: [
+    provideHttpClient(withFetch()),
+    provideRouter(routes, withComponentInputBinding()),
+    importProvidersFrom(
+      BrowserModule,
+      HttpClientModule,
+      MatSnackBarModule,
+      KeycloakAngularModule,
+      // NgbModule
+    ),
+    // provideAnimations(),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      deps: [KeycloakService],
+      multi: true,
+    },
+  ],
 };
--- Boersen-Party-FrontEnd/src/app/app.routes.ts
+++ Boersen-Party-FrontEnd/src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { UserComponent } from './user/user.component';
 import {LandingPageComponent} from './landing-page/landing-page.component';
 import {PersonalComponent} from './personal/personal.component';
 import {DisplayComponent} from './display/display.component';
+import {AuthGuard} from './guard/auth.guard';
 
 export const routes: Routes = [
   { path: '', title: 'Stock-Party', component: LandingPageComponent  },
-  { path: 'veranstalter', title: 'Stock-Party', component: VeranstalterComponent},
+  { path: 'veranstalter', title: 'Stock-Party', component: VeranstalterComponent, canActivate: [AuthGuard]},
   { path: 'user', title: 'Stock-Party', component: UserComponent},
-  { path: 'personal', title: 'Stock-Party', component: PersonalComponent},
+  { path: 'personal', title: 'Stock-Party', component: PersonalComponent, canActivate: [AuthGuard]},
   { path: 'display', title: 'Stock-Party', component: DisplayComponent},
 ];
 
