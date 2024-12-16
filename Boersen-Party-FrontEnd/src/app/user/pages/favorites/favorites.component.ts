c3b37ffcab954b68f170991e7e99caf32d3db2c2
T <tamoit02@hs-esslingen.de>
2024-12-16 20:09:26 +0100

    Favorite Page
    Co-Authored-By: Dominik <147036891+doeiit00@users.noreply.github.com>

--- Boersen-Party-FrontEnd/src/app/user/pages/favorites/favorites.component.ts
+++ Boersen-Party-FrontEnd/src/app/user/pages/favorites/favorites.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {ToolbarComponent} from '../../components/toolbar/toolbar.component';
+import {FavoriteItemComponent} from '../../components/favorite-item/favorite-item.component';
 
 @Component({
   selector: 'app-favoites',
   standalone: true,
-  imports: [],
+  imports: [
+    ToolbarComponent, FavoriteItemComponent
+  ],
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.css'
 })
--- Boersen-Party-FrontEnd/src/app/user/user.component.ts
+++ Boersen-Party-FrontEnd/src/app/user/user.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import {DrinkPriceOverviewUserComponent} from './components/drink-price-overview-user/drink-price-overview-user.component';
 import {GerneralStatisticsUserComponent} from './components/gerneral-statistics-user/gerneral-statistics-user.component';
-import {FavoriteItemComponent} from './components/favorite-item/favorite-item.component';
 import {ToolbarComponent} from './components/toolbar/toolbar.component';
 import {RouterOutlet} from '@angular/router';
+import {FavoriteItemHomeComponent} from './components/favorite-item/favorite-item-home/favorite-item-home.component';
 
 @Component({
   standalone: true,
   selector: 'app-user',
-  imports: [DrinkPriceOverviewUserComponent, GerneralStatisticsUserComponent, FavoriteItemComponent, ToolbarComponent, RouterOutlet],
+  imports: [DrinkPriceOverviewUserComponent, GerneralStatisticsUserComponent, FavoriteItemHomeComponent, ToolbarComponent, RouterOutlet],
   templateUrl: `./user.component.html`,
   styleUrls: ['./user.component.css']
 })
