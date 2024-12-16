40c15311b7cd424b44c339d90bce582752c2a095
selhdr <selinhidiroglu@web.de>
2024-12-16 16:32:24 +0100

    Branch and Grid

--- Boersen-Party-FrontEnd/src/app/app.routes.ts
+++ Boersen-Party-FrontEnd/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { UserComponent } from './user/user.component';
 import {LandingPageComponent} from './landing-page/landing-page.component';
 import {PersonalComponent} from './personal/personal.component';
 import {DisplayComponent} from './display/display.component';
+import {AdminComponent} from './admin/admin.component';
 
 export const routes: Routes = [
   { path: '', title: 'Stock-Party', component: LandingPageComponent  },
@@ -11,5 +12,6 @@ export const routes: Routes = [
   { path: 'user', title: 'Stock-Party', component: UserComponent},
   { path: 'personal', title: 'Stock-Party', component: PersonalComponent},
   { path: 'display', title: 'Stock-Party', component: DisplayComponent},
+  { path: 'admin', title: 'Stock-Party', component: AdminComponent},
 ];
 
