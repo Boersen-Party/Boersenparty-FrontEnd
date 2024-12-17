c15694ebbf204b592529561c27c2eca381b5e78a
selhdr <selinhidiroglu@web.de>
2024-12-17 17:35:35 +0100

    Site nearly completed

--- Boersen-Party-FrontEnd/src/app/admin/error-logs/error-logs.component.ts
+++ Boersen-Party-FrontEnd/src/app/admin/error-logs/error-logs.component.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: './error-logs.component.html',
   styleUrl: './error-logs.component.css'
 })
-export class ErrorLogsComponent {
 
+export class ErrorLogsComponent {
+  handleButtonClick(logType: string): void {
+    console.log(`${logType} clicked`);
+    // Füge hier die Logik hinzu, um die Logs anzuzeigen oder Daten zu laden
+  }
 }
