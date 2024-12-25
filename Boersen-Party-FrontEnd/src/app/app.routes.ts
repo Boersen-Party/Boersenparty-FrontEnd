3bcf7cb18e339236b45bda1b93b3dcb4845f9105
T <tamoit02@hs-esslingen.de>
2024-12-25 15:00:33 +0100

    Maybe Pipefix
    Co-Authored-By: Dominik <147036891+doeiit00@users.noreply.github.com>

--- Boersen-Party-FrontEnd/src/app/app.routes.ts
+++ Boersen-Party-FrontEnd/src/app/app.routes.ts
@@ -10,8 +10,7 @@ import { AuthGuard } from './services/auth.guard';
 
 export const routes: Routes = [
   { path: '', title: 'Stock-Party', component: LandingPageComponent  },
-  { path: 'veranstalter', title: 'Stock-Party', component: VeranstalterComponent, canActivate: [AuthGuard],
-    data: { roles: ['veranstalter'] }},
+  { path: 'veranstalter', title: 'Stock-Party', component: VeranstalterComponent}, //add canActivate: [AuthGuard], data: { roles: ['veranstalter'] }
   { path: 'user',
     title: 'Stock-Party',
     children: [
@@ -21,7 +20,6 @@ export const routes: Routes = [
       //{ path: 'favorites', title: 'Favourites', component: FavoritesComponent},
     ]
   },
-  { path: 'personal', title: 'Stock-Party', component: PersonalComponent, canActivate: [AuthGuard],
-    data: { roles: ['personal'] }},
+  { path: 'personal', title: 'Stock-Party', component: PersonalComponent}, // add canActivate: [AuthGuard], data: { roles: ['personal'] }},
   { path: 'display', title: 'Stock-Party', component: DisplayComponent},
 ];
