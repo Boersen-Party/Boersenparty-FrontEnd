c3feed7c92acac8f135cb5a5d64bf4d70010cfb2
T <tamoit02@hs-esslingen.de>
2024-12-25 14:35:49 +0100

    Keycloak Maybe
    Co-Authored-By: Dominik <147036891+doeiit00@users.noreply.github.com>

--- Boersen-Party-FrontEnd/src/app/app.config.ts
+++ Boersen-Party-FrontEnd/src/app/app.config.ts
@@ -1,35 +1,36 @@
-import { ApplicationConfig, APP_INITIALIZER, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { BrowserModule,provideAnimations } from '@angular/platform-browser'; // add provideAnimations
-import { HttpClientModule } from '@angular/common/http';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { KeycloakAngularModule } from 'keycloak-angular';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; // Konflikte mit Angular 18
-
-import { routes } from './app.routes';
+import { APP_INITIALIZER } from '@angular/core';
 
 import { KeycloakService } from 'keycloak-angular';
-import { initializeKeycloak } from './init/keycloak-init.factory';
+
+// Services
+import { initializer } from './services/keycloak-init';
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './services/auth.guard';
+import { CommonService } from './services/common.service';
+
+// Routes
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withFetch()),
-    provideRouter(routes, withComponentInputBinding()),
-    importProvidersFrom(
-      BrowserModule,
-      HttpClientModule,
-      MatSnackBarModule,
-      KeycloakAngularModule,
-      NgbModule
-    ),
-    provideAnimations(),
+    provideHttpClient(withFetch()), // HTTP client
+    provideRouter(routes, withComponentInputBinding()), // Routes
+
+    // Keycloak initialization using APP_INITIALIZER
     {
       provide: APP_INITIALIZER,
-      useFactory: initializeKeycloak,
+      useFactory: initializer,
       deps: [KeycloakService],
       multi: true,
     },
+
+    // Register services
+    KeycloakService,
+    AuthService,
+    AuthGuard,
+    CommonService,
   ],
 };
--- Boersen-Party-FrontEnd/src/app/app.routes.ts
+++ Boersen-Party-FrontEnd/src/app/app.routes.ts
@@ -1,16 +1,27 @@
 import { Routes } from '@angular/router';
 import { VeranstalterComponent } from './veranstalter/veranstalter.component';
 import { UserComponent } from './user/user.component';
-import {LandingPageComponent} from './landing-page/landing-page.component';
-import {PersonalComponent} from './personal/personal.component';
-import {DisplayComponent} from './display/display.component';
-import {AuthGuard} from './guard/auth.guard';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PersonalComponent } from './personal/personal.component';
+import { DisplayComponent } from './display/display.component';
+//import { FavoritesComponent } from './user/pages/favorites/favorites.component';
+//import { MenuComponent } from './user/pages/menu/menu.component';
+import { AuthGuard } from './services/auth.guard';
 
 export const routes: Routes = [
   { path: '', title: 'Stock-Party', component: LandingPageComponent  },
-  { path: 'veranstalter', title: 'Stock-Party', component: VeranstalterComponent, canActivate: [AuthGuard]},
-  { path: 'user', title: 'Stock-Party', component: UserComponent},
-  { path: 'personal', title: 'Stock-Party', component: PersonalComponent, canActivate: [AuthGuard]},
+  { path: 'veranstalter', title: 'Stock-Party', component: VeranstalterComponent, canActivate: [AuthGuard],
+    data: { roles: ['veranstalter'] }},
+  { path: 'user',
+    title: 'Stock-Party',
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', title: 'Home', component: UserComponent },
+      //{ path: 'menu', title: 'Speisekarte', component: MenuComponent},
+      //{ path: 'favorites', title: 'Favourites', component: FavoritesComponent},
+    ]
+  },
+  { path: 'personal', title: 'Stock-Party', component: PersonalComponent, canActivate: [AuthGuard],
+    data: { roles: ['personal'] }},
   { path: 'display', title: 'Stock-Party', component: DisplayComponent},
 ];
-
