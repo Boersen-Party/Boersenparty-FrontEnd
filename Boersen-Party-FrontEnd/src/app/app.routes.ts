e99d0f90e6265c06020d8bc2e2a7beb21ddc2532
Kneisesch <doeiit00@hs-esslingen.de>
2024-12-26 16:40:07 +0100

    fix maybe
    Co-Authored-By: T <149052166+tamoit02@users.noreply.github.com>

--- Boersen-Party-FrontEnd/src/app/app.routes.ts
+++ Boersen-Party-FrontEnd/src/app/app.routes.ts
@@ -4,24 +4,22 @@ import { UserComponent } from './user/user.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { PersonalComponent } from './personal/personal.component';
 import { DisplayComponent } from './display/display.component';
+import { AuthGuard } from './services/auth.guard';
+import { AccessDeniedComponent } from './access-denied/access-denied.component';
 //import { FavoritesComponent } from './user/pages/favorites/favorites.component';
 //import { MenuComponent } from './user/pages/menu/menu.component';
-import { AuthGuard } from './services/auth.guard';
-import {AccessDeniedComponent} from './access-denied/access-denied.component';
 
 export const routes: Routes = [
-  { path: '', title: 'Stock-Party', component: LandingPageComponent  },
-  { path: 'veranstalter', title: 'Stock-Party', component: VeranstalterComponent}, //add canActivate: [AuthGuard], data: { roles: ['veranstalter'] }
-  { path: 'user',
-    title: 'Stock-Party',
-    children: [
+  { path: '', title: 'Stock-Party', component: LandingPageComponent },
+  { path: 'veranstalter', title: 'Veranstalter-Bereich', component: VeranstalterComponent, canActivate: [AuthGuard], data: { roles: ['veranstalter'] } },
+  { path: 'user', title: 'User-Bereich', children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', title: 'Home', component: UserComponent },
-      //{ path: 'menu', title: 'Speisekarte', component: MenuComponent},
-      //{ path: 'favorites', title: 'Favourites', component: FavoritesComponent},
+      { path: 'home', title: 'User Home', component: UserComponent },
+      //{ path: 'menu', title: 'Speisekarte', component: MenuComponent },
+      // { path: 'favorites', title: 'Favourites', component: FavoritesComponent },
     ]
   },
-  { path: 'personal', title: 'Stock-Party', component: PersonalComponent}, // add canActivate: [AuthGuard], data: { roles: ['personal'] }},
-  { path: 'display', title: 'Stock-Party', component: DisplayComponent},
-  { path: 'access-denied', component : AccessDeniedComponent}
+  { path: 'personal', title: 'Personal-Bereich', component: PersonalComponent, canActivate: [AuthGuard], data: { roles: ['personal'] } },
+  { path: 'display', title: 'Display', component: DisplayComponent },
+  { path: 'access-denied', title: 'Access Denied', component: AccessDeniedComponent }
 ];
