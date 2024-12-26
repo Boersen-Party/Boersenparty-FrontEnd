f146fae5e132bd4ea5811db71199a96d1ff774bc
T <tamoit02@hs-esslingen.de>
2024-12-26 15:04:52 +0100

    Pipe Fix I hope
    Co-Authored-By: Dominik <147036891+doeiit00@users.noreply.github.com>

--- Boersen-Party-FrontEnd/src/app/app.routes.ts
+++ Boersen-Party-FrontEnd/src/app/app.routes.ts
@@ -4,22 +4,17 @@ import { UserComponent } from './user/user.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { PersonalComponent } from './personal/personal.component';
 import { DisplayComponent } from './display/display.component';
-//import { FavoritesComponent } from './user/pages/favorites/favorites.component';
-//import { MenuComponent } from './user/pages/menu/menu.component';
-import { AuthGuard } from './services/auth.guard';
 
 export const routes: Routes = [
   { path: '', title: 'Stock-Party', component: LandingPageComponent  },
-  { path: 'veranstalter', title: 'Stock-Party', component: VeranstalterComponent}, //add canActivate: [AuthGuard], data: { roles: ['veranstalter'] }
+  { path: 'veranstalter', title: 'Stock-Party', component: VeranstalterComponent},
   { path: 'user',
     title: 'Stock-Party',
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', title: 'Home', component: UserComponent },
-      //{ path: 'menu', title: 'Speisekarte', component: MenuComponent},
-      //{ path: 'favorites', title: 'Favourites', component: FavoritesComponent},
     ]
   },
-  { path: 'personal', title: 'Stock-Party', component: PersonalComponent}, // add canActivate: [AuthGuard], data: { roles: ['personal'] }},
+  { path: 'personal', title: 'Stock-Party', component: PersonalComponent},
   { path: 'display', title: 'Stock-Party', component: DisplayComponent},
 ];
