bb876c75a069eb1ea5f7823ebaaddcb2e619fd41
Dominik <147036891+doeiit00@users.noreply.github.com>
2024-12-26 18:15:52 +0100

    Revert "Keycloak merge"

--- Boersen-Party-FrontEnd/src/environments/keycloak.config.ts
+++
@@ -1,9 +0,0 @@
-import {KeycloakConfig} from 'keycloak-js';
-
-const keycloakConfig: KeycloakConfig = {
-  url: 'https://idp.stock-party.live/admin/stock-party/console/',
-  realm: 'stock-party',
-  clientId: 'frontend-client-dev',
-};
-
-export default keycloakConfig;
