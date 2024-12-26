da9793336a980948e3ea87ecad7937785425aa40
Kneisesch <doeiit00@hs-esslingen.de>
2024-12-26 17:17:07 +0100

    reset
    Co-Authored-By: T <149052166+tamoit02@users.noreply.github.com>

---
+++ Boersenparty-FrontEnd/Boersen-Party-FrontEnd/src/app/user/user.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-user',
+  imports: [],
+  template: ``,
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent {
+}
