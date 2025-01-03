2ebba178ce67e79e9dc01d21d0af5ed44983b051
mfnomad <mfnomad@gmail.com>
2025-01-03 12:49:38 +0100

    injected services into display

--- Boersen-Party-FrontEnd/src/app/display/display.component.ts
+++ Boersen-Party-FrontEnd/src/app/display/display.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, effect } from '@angular/core';
 import { PartyHeaderComponent } from './party-header/party-header.component';
 import { SwipeableStatsgridComponent } from './swipeable-statsgrid/swipeable-statsgrid.component';
+import { PartyServiceService } from '../services/party-service.service';
+import { ProductService } from '../services/products.service';
+import { Party } from '../_model/party';
+import { Product } from '../_model/product';
 
 @Component({
   standalone: true,
@@ -10,5 +14,22 @@ import { SwipeableStatsgridComponent } from './swipeable-statsgrid/swipeable-sta
   styleUrl: './display.component.css'
 })
 export class DisplayComponent {
+    parties: Party[] = [];
+    products: Product[] = [];
+
+  
+
+  constructor(private partyService: PartyServiceService, private productService: ProductService) {
+    effect(() => {
+      this.parties = this.partyService.parties();
+      console.log("Updated parties in DisplayComponent:", this.parties);
+      this.products = this.productService.products();
+      console.log("Updated products in DisplayComponent:", this.products);
+    });
+
+  }
+
+
+   
 
 }
--- Boersen-Party-FrontEnd/src/app/veranstalter/created-party-tab/created-party-tab.component.ts
+++ Boersen-Party-FrontEnd/src/app/veranstalter/created-party-tab/created-party-tab.component.ts
@@ -9,11 +9,13 @@ import { Party } from '../../_model/party';
   templateUrl: './created-party-tab.component.html',
   styleUrl: './created-party-tab.component.css'
 })
-export class CreatedPartyTabComponent {
-    parties: Party[] = [];
+export class CreatedPartyTabComponent {  
+  parties: Party[] = [];
 
 
   constructor(private partyService: PartyServiceService){
+    //maybe optimizie this, since there is just 1 party active at a time - remove?
+    // why fetch parties constantly if there is just 1
     effect(() => {
       this.parties = this.partyService.parties();
       console.log("Updated parties in CreatedPartyTabComponent:", this.parties);
