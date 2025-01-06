8b64a6f4cb0594223cc2bcf7c61ef345e17fd206
T <tamoit02@hs-esslingen.de>
2025-01-06 16:11:10 +0100

    Keycloak not working yet
    Co-Authored-By: Dominik <147036891+doeiit00@users.noreply.github.com>

--- Boersen-Party-FrontEnd/src/app/veranstalter/logout-btn/logout-btn.component.ts
+++ Boersen-Party-FrontEnd/src/app/veranstalter/logout-btn/logout-btn.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-logout-btn',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './logout-btn.component.css'
 })
 export class LogoutBtnComponent {
+  constructor(private keycloakService: KeycloakService) {}
+
+  logout(): void {
+    const keycloakLogoutUrl = `http://localhost:4200/verantstalter`; //https://idp.stock-party.live/realms/stock-party/protocol/openid-connect/auth
+    this.keycloakService.logout(keycloakLogoutUrl);
+  }
 
 }
