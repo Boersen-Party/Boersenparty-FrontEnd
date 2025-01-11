57c88a2a88ff7f41b442cdd0f04e90bf934349b1
mfnomad <mfnomad@gmail.com>
2025-01-11 16:52:45 +0100

    event button adjust

--- Boersen-Party-FrontEnd/src/app/personal/sub-total/sub-total.component.ts
+++ Boersen-Party-FrontEnd/src/app/personal/sub-total/sub-total.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import {CurrencyPipe, NgForOf} from '@angular/common';
+import { OrderSelectionService } from '../../services/order-selection.service';
 
 @Component({
   selector: 'app-sub-total',
   standalone: true,
-  imports: [
-    CurrencyPipe,
-    NgForOf
-  ],
   templateUrl: './sub-total.component.html',
   styleUrl: './sub-total.component.css'
 })
-export class SubTotalComponent {
+  export class SubTotalComponent {
+    order = computed(() => this.orderSelectionService.selectedOrder());
+  
+    constructor(private orderSelectionService: OrderSelectionService) {}
+  }
 
-}
