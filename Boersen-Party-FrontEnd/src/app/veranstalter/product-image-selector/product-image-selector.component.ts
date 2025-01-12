c84943c3eebf8054b3a80d97171a7ffa0bbf0824
T <tamoit02@hs-esslingen.de>
2025-01-12 21:09:24 +0100

    New Product Pictures
    Co-Authored-By: Dominik <147036891+doeiit00@users.noreply.github.com>

--- Boersen-Party-FrontEnd/src/app/veranstalter/product-image-selector/product-image-selector.component.ts
+++ Boersen-Party-FrontEnd/src/app/veranstalter/product-image-selector/product-image-selector.component.ts
@@ -18,7 +18,19 @@ export class ProductImageSelectorComponent {
     'https://cdn-icons-png.flaticon.com/512/1178/1178890.png',
     'https://cdn-icons-png.flaticon.com/512/988/988934.png',
     'https://cdn-icons-png.flaticon.com/512/1425/1425548.png',
-    'https://cdn-icons-png.flaticon.com/512/2331/2331996.png'
+    'https://cdn-icons-png.flaticon.com/512/2331/2331996.png',
+    'https://cdn-icons-png.flaticon.com/128/1404/1404945.png',
+    'https://cdn-icons-png.flaticon.com/128/5787/5787016.png',
+    'https://cdn-icons-png.flaticon.com/128/590/590717.png',
+    'https://cdn-icons-png.flaticon.com/128/7511/7511906.png',
+    'https://cdn-icons-png.flaticon.com/128/7511/7511906.png',
+    'https://cdn-icons-png.flaticon.com/128/3174/3174535.png',
+    'https://cdn-icons-png.flaticon.com/128/3174/3174535.png',
+    'https://cdn-icons-png.flaticon.com/128/920/920564.png',
+    'https://cdn-icons-png.flaticon.com/128/3329/3329302.png',
+    'https://cdn-icons-png.flaticon.com/128/920/920562.png',
+    'https://cdn-icons-png.flaticon.com/128/1046/1046782.png',
+    'https://cdn-icons-png.flaticon.com/128/4265/4265446.png'
   ];
 
 
@@ -26,7 +38,7 @@ export class ProductImageSelectorComponent {
     this.selectedImage = imageUrl;
   }
 
-  
+
   confirmImageSelection() {
     this.imageSelected.emit(this.selectedImage);
     this.closeImageSelector();
