724544a19991e4780971340ef1426d19cada2bcf
Dominik <147036891+doeiit00@users.noreply.github.com>
2025-01-13 22:55:12 +0100

    Update for brod
    Co-Authored-By: T <149052166+tamoit02@users.noreply.github.com>

--- Boersen-Party-FrontEnd/src/environments/keycloak.config.ts
+++ Boersen-Party-FrontEnd/src/environments/keycloak.config.ts
@@ -3,7 +3,7 @@ import {KeycloakConfig} from 'keycloak-js';
 const keycloakConfig: KeycloakConfig = {
   url: 'https://idp.stock-party.live',
   realm: 'stock-party',
-  clientId: 'Stock-Party-Client',
+  clientId: 'stock-party-client',
 };
 
 export default keycloakConfig;
