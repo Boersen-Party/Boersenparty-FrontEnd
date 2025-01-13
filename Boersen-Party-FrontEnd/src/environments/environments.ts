3bdab85d835d4e13fcfa2d5162204e052accb7f9
Dominik <147036891+doeiit00@users.noreply.github.com>
2025-01-13 21:54:09 +0100

    changes for redirection
    Co-Authored-By: T <149052166+tamoit02@users.noreply.github.com>

--- Boersen-Party-FrontEnd/src/environments/environments.ts
+++ Boersen-Party-FrontEnd/src/environments/environments.ts
@@ -5,5 +5,5 @@ export const environment = {
   apiUrl: "/api",
   keycloak: keycloakConfig,
   //baseUrl: 'http://localhost:4200/',
-  baseUrl: 'https://dev.stock-party.live/' // points to springboot, also add this url in the proxy config
+  baseUrl: 'https://stock-party.live/'
 };
