0e7c745352c0ea78919d75145579068c55288875
Dominik <147036891+doeiit00@users.noreply.github.com>
2025-01-13 19:09:56 +0100

    dacing hot dog
    Co-Authored-By: T <149052166+tamoit02@users.noreply.github.com>

--- Boersen-Party-FrontEnd/src/app/personal/event-button/event-button.component.ts
+++ Boersen-Party-FrontEnd/src/app/personal/event-button/event-button.component.ts
@@ -12,20 +12,29 @@ import { EventService } from '../../services/event.service';
 export class EventButtonComponent {
 //ironically probably swap name EventButtonComponent with EventTriggerWindow
   events: PartyEvent[] = [];
-   
+
   constructor(private eventService: EventService) {
     effect(() => {
       this.events = this.eventService.events();
       console.log("Updated parties in EventButtonComponent:", this.events);
-      
+
     });
 }
+
+showGif = false;
 
 triggerEvent(eventId: number) {
   if (eventId === 0) {
     console.warn('Cannot trigger event: Invalid event ID.');
     return;
-  } 
+  }
+
+  this.showGif = true;
+
+  setTimeout(() => {
+    this.showGif = false;
+    console.log('GIF ausgeblendet');
+  }, 5000);
 
   console.log('Triggering event with ID:', eventId);
   this.eventService.triggerEvent(eventId);
