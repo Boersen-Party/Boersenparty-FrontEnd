4265df757c85b3a6b3aef48e2d9b5688d4d2b543
T <tamoit02@hs-esslingen.de>
2025-01-13 22:22:18 +0100

    Dev Fix
    Co-Authored-By: Dominik <147036891+doeiit00@users.noreply.github.com>

--- Boersen-Party-FrontEnd/src/environments/keycloak.config.ts
+++ Boersen-Party-FrontEnd/src/environments/keycloak.config.ts
@@ -3,7 +3,7 @@ import {KeycloakConfig} from 'keycloak-js';
 const keycloakConfig: KeycloakConfig = {
   url: 'https://idp.stock-party.live',
   realm: 'stock-party',
-  clientId: 'Stock-Party-Client',
+  clientId: 'frontend-client-dev',
 };
 
 export default keycloakConfig;
